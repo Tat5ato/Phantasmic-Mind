8cb502f094fc67d8c5b765fb42032b70b5bea264
TechToolbox <nkomarn@hotmail.com>
2017-09-21 19:50:15 -0700

    Add files via upload

--- Henry.cs
+++ Henry.cs
@@ -4,22 +4,22 @@ using UnityEngine;
 
 public class Henry : MonoBehaviour{
 
-	//variables
-	public double attack = 0;
+	// Variables
+	public float attack = 0;
 	public int HP = 100;
 	public float defense = 10f;
 	private int attackType = 0;
 	private int defenseType = 0;
-	public double enemyTestHP = 100f;
+	public float enemyTestHP = 100f;
 	public bool AttackChange = false;
 
-	//switches for items or buffs
+    // Main function
 	void Update(){
 
 		//declared up before the void update, but you cant use random.range outside a void update
-		double attack = Random.Range(7f, 10f);
+		float attack = Random.Range(7f, 10f);
 
-		//-insert definition here-
+		// Defense Type Switch
 		switch(defenseType){
 		case 1:
 			defense = (defense - 1.33f);
@@ -32,15 +32,15 @@ public class Henry : MonoBehaviour{
 		//cases represent weapons (or mabye buffs) ex. default is fists, case 1 is a shovel, case 2 is a sword
 		switch (attackType) {
 		case 1:
-			attack = (attack * 1.5);
+			attack = (attack * 1.5f);
 			AttackChange = true;
 			break;
 		case 2:
-			attack = (attack * 2);
+			attack = (attack * 2f);
 			AttackChange = true;
 			break;
 		default:
-			attack = Random.Range (7, 10);
+			attack = Random.Range (7f, 10f);
 			AttackChange = true;
 			break;
 		}
