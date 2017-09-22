396784ed0d862dc63b7dd7cf3a51d160d0a62e23
TechToolbox <nkomarn@hotmail.com>
2017-09-21 19:53:16 -0700

    Added Powerups and Fixed Floats

--- Henry.cs
+++ Henry.cs
@@ -4,52 +4,64 @@ using UnityEngine;
 
 public class Henry : MonoBehaviour{
 
+	// ADD - Check if item is in inventory
+
 	// Variables
-	public float attack = 0;
-	public int HP = 100;
+	public float attack = 0f;
+	public float HP = 100f;
 	public float defense = 10f;
 	private int attackType = 0;
 	private int defenseType = 0;
 	public float enemyTestHP = 100f;
-	public bool AttackChange = false;
+	public float fearGuage = 30f;
+
+	// Temporary
+	public bool antiDepression = false;
+	public bool alcohol = false;
 
     // Main function
-	void Update(){
+	void Update() {
 
-		//declared up before the void update, but you cant use random.range outside a void update
+		// Set Attack Value
 		float attack = Random.Range(7f, 10f);
+
+		// Apply buffs if antiDepression of alcohol in inventory
+		if (antiDepression == true) {
+			// Apply attack buff 2X & defense 1.33
+		}
+			
+		if (alcohol == true) {
+			// Apply attack buff 2X & defense 1.33
+		}
 
 		// Defense Type Switch
 		switch(defenseType){
-		case 1:
-			defense = (defense - 1.33f);
-			break;
 		default:
 			defense = 10f;
 			break;
+		case 1:
+			defense = (defense - 1.33f);
+			break;
 		}
 
-		//cases represent weapons (or mabye buffs) ex. default is fists, case 1 is a shovel, case 2 is a sword
+		// AttackType switch
 		switch (attackType) {
+		default:
+			attack = Random.Range (7f, 10f);
+			break;
 		case 1:
 			attack = (attack * 1.5f);
-			AttackChange = true;
 			break;
 		case 2:
 			attack = (attack * 2f);
-			AttackChange = true;
-			break;
-		default:
-			attack = Random.Range (7f, 10f);
-			AttackChange = true;
 			break;
 		}
 	}
 
 
-	//buffs
 	//fear
 	//GUI
+
 
 
 }
