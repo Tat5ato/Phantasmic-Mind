3ea571322b0d56885664dceb4b6b228559346351
Vance Winstead <32073638+bery20@users.noreply.github.com>
2017-10-25 10:35:05 -0700

    Update and rename Camera.cs to CameraFollowPlayer.cs

--- src/scripts/player-scripts/CameraFollowPlayer.cs
+++ src/scripts/player-scripts/CameraFollowPlayer.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Camera : MonoBehaviour {
+public class CameraFollowPlayer : MonoBehaviour {
 
 	[SerializeField]
 	GameObject player; //main character being followed
