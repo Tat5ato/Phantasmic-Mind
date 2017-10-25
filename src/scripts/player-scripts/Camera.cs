9761ba9e8206aefed00fd42d73e3b4f4eab8cef2
Vance Winstead <32073638+bery20@users.noreply.github.com>
2017-10-25 10:34:01 -0700

    Update Camera.cs

--- src/scripts/player-scripts/Camera.cs
+++ src/scripts/player-scripts/Camera.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CameraFollowPlayer : MonoBehaviour {
+public class Camera : MonoBehaviour {
 
 	[SerializeField]
 	GameObject player; //main character being followed
