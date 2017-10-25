149a9e45d522227a4ff6609096672ce0ec34b40a
Vance Winstead <32073638+bery20@users.noreply.github.com>
2017-10-25 11:52:29 -0700

    Update PlayerMovement.cs
    Player movement only occurs in four directions now

--- src/scripts/player-scripts/PlayerMovement.cs
+++ src/scripts/player-scripts/PlayerMovement.cs
@@ -5,32 +5,63 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour {
 
 	[SerializeField]
-	private float speed = 1;
+	private float speed = 0.1f;
 
 	private Rigidbody2D rb2d;
+	private bool movingHorizontal;
+	private bool movingVertical;
+
 
 	// Use this for initialization
 	void Start () {
 
 		//Get and store a reference to the Rigidbody2D component so that we can access it.
 		rb2d = GetComponent<Rigidbody2D> ();
-		
+		movingHorizontal = true;
+		movingVertical = true;
+
 	}
-	
+
 	// Update is called once per frame
 	void FixedUpdate () {
 
-		//Store the current horizontal input in the float moveHorizontal.
-		float moveHorizontal = Input.GetAxis ("Horizontal");
+		float moveHorizontal = 0;
+		float moveVertical = 0;
 
-		//Store the current vertical input in the float moveVertical.
-		float moveVertical = Input.GetAxis ("Vertical");
+		//Store the current horizontal input in the float moveHorizontal.
+
+		//#region RegionName
+		if (
+			Input.GetKeyDown ("up") || Input.GetKeyDown ("w") || Input.GetKeyDown ("down") || Input.GetKeyDown ("s")
+			|| Input.GetKeyUp ("right") || Input.GetKeyUp ("a") || Input.GetKeyUp ("left") || Input.GetKeyUp ("d")
+		) {
+			movingVertical = true;
+			movingHorizontal = false;
+		}
+
+		if (
+			Input.GetKeyDown ("right") || Input.GetKeyDown ("a") || Input.GetKeyDown ("left") || Input.GetKeyDown ("d")
+			|| Input.GetKeyUp ("up") || Input.GetKeyUp ("w") || Input.GetKeyUp ("down") || Input.GetKeyUp ("s")
+		) {
+			movingVertical = false ;
+			movingHorizontal = true;
+		}
+		//#endregion
+
+		if (movingHorizontal) {
+			moveHorizontal = Input.GetAxis ("Horizontal");
+		}
+			
+		if (movingVertical) {
+			moveVertical = Input.GetAxis ("Vertical");
+		}
+
 
 		//Store movement in a 2d vector
-		Vector2 movement = new Vector2(moveHorizontal, moveVertical);
+		Vector2 movement = new Vector2(moveHorizontal, moveVertical); 
 
 		//move the player
 		rb2d.MovePosition (rb2d.position + movement * speed);
 	}
-		
+
 }
