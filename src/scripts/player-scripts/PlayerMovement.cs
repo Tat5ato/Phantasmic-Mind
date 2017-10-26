b91de517948fe2103f3c2bfaf57aa2756c6d48f2
Vance Winstead <32073638+bery20@users.noreply.github.com>
2017-10-26 15:45:00 -0700

    Update PlayerMovement.cs
    This should be the final update to the player movement script

--- src/scripts/player-scripts/PlayerMovement.cs
+++ src/scripts/player-scripts/PlayerMovement.cs
@@ -8,8 +8,11 @@ public class PlayerMovement : MonoBehaviour {
 	private float speed = 0.1f;
 
 	private Rigidbody2D rb2d;
-	private bool movingHorizontal;
-	private bool movingVertical;
+
+	private bool movingUp;
+	private bool movingDown;
+	private bool movingLeft;
+	private bool movingRight;
 
 
 	// Use this for initialization
@@ -17,8 +20,11 @@ public class PlayerMovement : MonoBehaviour {
 
 		//Get and store a reference to the Rigidbody2D component so that we can access it.
 		rb2d = GetComponent<Rigidbody2D> ();
-		movingHorizontal = true;
-		movingVertical = true;
+
+		movingUp = true;
+		movingDown = true;
+		movingLeft = true;
+		movingRight = true;
 
 	}
 
@@ -28,33 +34,55 @@ public class PlayerMovement : MonoBehaviour {
 		float moveHorizontal = 0;
 		float moveVertical = 0;
 
-		//Store the current horizontal input in the float moveHorizontal.
 
-		//#region RegionName
-		if (
-			Input.GetKeyDown ("up") || Input.GetKeyDown ("w") || Input.GetKeyDown ("down") || Input.GetKeyDown ("s")
-			|| Input.GetKeyUp ("right") || Input.GetKeyUp ("a") || Input.GetKeyUp ("left") || Input.GetKeyUp ("d")
-		) {
-			movingVertical = true;
-			movingHorizontal = false;
+		//Checks to see if moving up
+		if (Input.GetKeyDown ("up") || Input.GetKeyDown ("w")
+			|| ((Input.GetKeyUp ("right") || Input.GetKeyUp ("d") || Input.GetKeyUp ("left") || Input.GetKeyUp ("a") || Input.GetKeyUp ("down") || Input.GetKeyUp ("s")) 
+				&& (Input.GetKey ("up") || Input.GetKey ("w")))) {
+			movingUp = true;
+			movingDown = false;
+			movingLeft = false;
+			movingRight = false;
 		}
 
-		if (
-			Input.GetKeyDown ("right") || Input.GetKeyDown ("a") || Input.GetKeyDown ("left") || Input.GetKeyDown ("d")
-			|| Input.GetKeyUp ("up") || Input.GetKeyUp ("w") || Input.GetKeyUp ("down") || Input.GetKeyUp ("s")
-		) {
-			movingVertical = false ;
-			movingHorizontal = true;
+		//Checks to see if moving down
+		if (Input.GetKeyDown ("down") || Input.GetKeyDown ("s")
+			|| ((Input.GetKeyUp ("right") || Input.GetKeyUp ("d") || Input.GetKeyUp ("left") || Input.GetKeyUp ("a") || Input.GetKeyUp ("up") || Input.GetKeyUp ("w")) 
+				&& (Input.GetKey ("down") || Input.GetKey ("s")))) {
+			movingUp = false;
+			movingDown = true;
+			movingLeft = false;
+			movingRight = false;
 		}
-		//#endregion
 
-		if (movingHorizontal) {
+		//Checks to see if moving left
+		if (Input.GetKeyDown ("left") || Input.GetKeyDown ("a")
+			|| ((Input.GetKeyUp ("right") || Input.GetKeyUp ("d") || Input.GetKeyUp ("up") || Input.GetKeyUp ("w") || Input.GetKeyUp ("down") || Input.GetKeyUp ("s")) 
+				&& (Input.GetKey ("left") || Input.GetKey ("a")))) {
+			movingUp = false;
+			movingDown = false;
+			movingLeft = true;
+			movingRight = false;
+		}
+
+		//Checks to see if moving right
+		if (Input.GetKeyDown ("right") || Input.GetKeyDown ("d")
+			|| ((Input.GetKeyUp ("left") || Input.GetKeyUp ("a") || Input.GetKeyUp ("up") || Input.GetKeyUp ("w") || Input.GetKeyUp ("down") || Input.GetKeyUp ("s")) 
+				&& (Input.GetKey ("right") || Input.GetKey ("d")))) {
+			movingUp = false;
+			movingDown = false;
+			movingLeft = false;
+			movingRight = true;
+		}
+
+
+		if (movingLeft || movingRight) {
 			moveHorizontal = Input.GetAxis ("Horizontal");
 		}
-			
-		if (movingVertical) {
+
+		if (movingUp || movingDown) {
 			moveVertical = Input.GetAxis ("Vertical");
-		}
+		} 
 
 
 		//Store movement in a 2d vector
