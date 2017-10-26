6c5adf0cdb6a39721ab4e6b03014c4402e8533b1
ZumbaMaster313 <32375483+ZumbaMaster313@users.noreply.github.com>
2017-10-26 15:15:49 -0700

    Update EnemyMovement.cs

--- src/scripts/env/EnemyMovement.cs
+++ src/scripts/env/EnemyMovement.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Movement : MonoBehaviour {
-
+// Made by Sebastian Carbonero and Adrien Cao
 	//Referenced Game Objects
 	public Transform Player;
 	public Transform NavPointA;
