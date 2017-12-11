4a486beca9dc78b741e704c55f8c1db02309fbff
ZumbaMaster313 <32375483+ZumbaMaster313@users.noreply.github.com>
2017-12-11 15:38:08 -0800

    Add files via upload
    The fear machine gives the player more fear as the player approaches something that they are scared of (zombies, clowns, etc.). The weather test randomly creates thunder when it is raining, since thunder is one of the player's fears too.

---
+++ src/scripts/player-scripts/FearMachine.cs
@@ -0,0 +1,93 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FearMachine : MonoBehaviour {
+	/*
+	public Transform Spider;
+	public Transform Water;
+	public Transform Blood;
+	public Transform Quadrupeds; // 4-Limbed Animals
+	public Transform Doll;
+	public Transform Thunder;
+	public Transform Ghosts;
+	public Transform Clowns;
+	public Transform Holes;
+*/
+	float maxRadius = 15.0f;
+	public string Playerfear;
+	public string Thunder;
+
+
+
+	public Transform player;
+
+	//FOR EXAMPLE ONLY
+	public int Damage;
+	public int DamageDoubled;
+	//------
+
+
+
+
+
+	//Henry playerInstance = GetComponent<Henry>();
+
+	// Use this for initialization
+	void Start () 
+	{
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Vector3.Distance (gameObject.transform.position, player.transform.position) <= maxRadius) {
+			if (gameObject.transform.name == Playerfear) {
+				if (gameObject.transform.tag == "Enemy") {
+					if (DamageDoubled != (Damage * 2)) {
+						DamageDoubled = (Damage * 2);
+						Debug.Log ("Damage of Enemy has been doubled");
+					}
+				}
+				//WE NEED A DISTINCT FEAR VALUE IN ORDER TO MODIFY ANYTHING
+				Debug.Log ("Fear is increasing...");
+			}
+		}
+
+
+	
+	}
+	// Fear of Spiders
+	public void Arachnophobia () 
+	{ 
+
+    }
+	public void Acrophobia () { // Fear of Heights
+
+	}
+	public void Aquaphobia () { // Fear of Water and Aquatic Things
+
+	}
+	public void Hemophobia () { // Fear of Blood
+
+	}
+	public void Zoophobia () { // Fear of 4-limbed animals
+
+	}
+	public void Pediophobia () { // Fear of Dolls
+
+	}
+	public void Astraphobia () { // Fear of Thunder/Lightning
+
+	}
+	public void Spectrophobia () { // Fear of Ghosts
+
+	}
+	public void Coulrophobia () { // Fear of Clowns
+
+	}
+	public void Trypophobia () { //Fear of Holes
+
+	}
+
+}
---
+++ src/scripts/player-scripts/WeatherTest.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeatherTest : MonoBehaviour {
+	public bool IsRaining;
+	public bool Thunder;
+	public int ThunderInterval;
+	// Use this for initialization
+	void Start () {
+		ThunderInterval = 3;
+		StartCoroutine (ThunderCode ());
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (IsRaining == true) {
+			Debug.Log ("Fear is increasing...");
+		}
+	}
+	IEnumerator ThunderCode(){
+		yield return new WaitForSeconds (ThunderInterval);
+		ThunderInterval = Random.Range (5, 20);
+		Debug.Log ("THUNDER");
+		StartCoroutine (ThunderCode ());
+
+	}
+}
